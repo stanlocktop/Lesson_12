6063b2869ad64bd2088f1c7649c6ecf3f15e7ea6
stanlocktop <wlad-test@ukr.net>
2023-10-14 15:58:22 +0300

    finish CheckingAccount.cs

--- Task1/CheckingAccount.cs
+++ Task1/CheckingAccount.cs
@@ -1,25 +1,56 @@
 namespace BankAccount
 {
-    internal class CheckingAccount : BankAccount
+    class CheckingAccount : BankAccount
     {
-        double overdraftLimit;
+        double overdraftLimit; // Ліміт овердрафту
+
         public CheckingAccount(string accountHolder, double balance, double overdraftLimit) : base(accountHolder, balance)
         {
             this.overdraftLimit = overdraftLimit;
         }
+
         public override double Deposit(double amount)
         {
-            throw new NotImplementedException();
-        }
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Внесено {amount:C}. Новий баланс: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Сума для внесення повинна бути більше нуля.");
+            }
 
-        public override void DisplayAccountInfo()
-        {
-            throw new NotImplementedException();
+            return balance;
         }
 
         public override double Withdraw(double amount)
         {
-            throw new NotImplementedException();
+            if (amount > 0)
+            {
+                if (balance - amount >= -overdraftLimit)
+                {
+                    balance -= amount;
+                    Console.WriteLine($"Знято {amount:C}. Новий баланс: {balance:C}");
+                }
+                else
+                {
+                    Console.WriteLine("Перевищено ліміт овердрафту.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Сума для зняття повинна бути більше нуля.");
+            }
+
+            return balance;
+        }
+
+        public override void DisplayAccountInfo()
+        {
+            Console.WriteLine($"Баланс: {balance:C}");
+            Console.WriteLine($"Власник рахунку: {accountHolder}");
+            Console.WriteLine($"Ліміт овердрафту: {overdraftLimit:C}");
         }
     }
 }
